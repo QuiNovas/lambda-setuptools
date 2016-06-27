902344a0bfe6e6729ce8eab0060a3546feaeec77
Joseph Wortmann <jwortmann@illumicare.com>
2016-06-27 17:17:10 -0500

    Initial check in

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as readme:
+    long_description = readme.read()
+
+setup(
+    name='lambda-setuptools',
+
+    version='0.1.0',
+
+    description='A Command extension to setuptools that allows building an AWS Lamba dist',
+    long_description=long_description,
+
+    url='https://github.com/illumicare/setuptools-lambda-dist-extension',
+
+    author='Joseph Wortmann',
+    author_email='joseph.wortmann@gmail.com',
+
+    license='APL 2.0',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        ],
+
+    keywords='setuptools extension',
+
+    install_requires=['setuptools'],
+
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+
+    entry_points={"distutils.commands": ['ldist = lambda_setuptools.ldist:LDist']}
+)
---
+++ src/lambda_setuptools/ldist.py
@@ -0,0 +1,79 @@
+import errno
+import os
+import shutil
+import zipfile
+
+from distutils import log
+from distutils.errors import DistutilsPlatformError, DistutilsInternalError
+from setuptools import Command
+from subprocess import Popen, PIPE
+
+
+class LDist(Command):
+
+    description = 'build a AWS Lambda compatible distribution'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # We must create a distribution to install first
+        # This is a short-cut to working with the actual build
+        # directory, or to using the 'install' command, which
+        # will generally only install a zipped egg
+        self.run_command('sdist')
+
+        # Install the package built by sdist
+        # (or bdist, or bdist_wheel, depending on how the user called setup.py
+        self._install_dist_package()
+
+        # Now build the lambda package
+        self._build_lambda_package()
+
+    def _build_lambda_package(self):
+        dist_name = '{}-lambda-{}.zip'.format(self.distribution.get_name(), self.distribution.get_version())
+        dist_dir = self.get_finalized_command('sdist').dist_dir
+        dist_path = os.path.join(dist_dir, dist_name)
+        if os.path.exists(dist_path):
+            os.remove(dist_path)
+        log.info('creating {}'.format(dist_path))
+        with zipfile.ZipFile(dist_path, 'w', zipfile.ZIP_DEFLATED) as zf:
+            abs_src = os.path.abspath(self._lambda_build_dir)
+            for root, _, files in os.walk(self._lambda_build_dir):
+                for filename in files:
+                    absname = os.path.abspath(os.path.join(root, filename))
+                    arcname = absname[len(abs_src) + 1:]
+                    log.debug('zipping {} as {}'.format((os.path.join(root, filename), arcname)))
+                    zf.write(absname, arcname)
+
+    def _install_dist_package(self):
+        # Get the name of the package that we just built
+        package_name = self.distribution.get_name()
+        # Get the dist directory that sdist put the package in
+        # Create the lambda build dir
+        self._lambda_build_dir = os.path.join('build', 'ldist-'+package_name)
+        try:
+            if os.path.exists(self._lambda_build_dir):
+                shutil.rmtree(self._lambda_build_dir)
+            log.info('creating {}'.format(self._lambda_build_dir))
+            os.makedirs(self._lambda_build_dir)
+        except OSError as exc:
+            if exc.errno == errno.EEXIST and os.path.isdir(self._lambda_build_dir):
+                pass
+            else:
+                raise DistutilsInternalError('{} already exists and is not a directory'.format(self._lambda_build_dir))
+        log.info('installing package {} into {}'.format(package_name, self._lambda_build_dir))
+        pip = Popen(['pip', 'install',
+                     '-f ', self.get_finalized_command('sdist').dist_dir,
+                     '-t', self._lambda_build_dir, package_name],
+                    stdout=PIPE, stderr=PIPE)
+        stdout, stderr = pip.communicate()
+        log.debug("pip stdout: {}".format(stdout))
+        log.debug("pip stderr: {}".format(stderr))
+
+        if pip.returncode is not 0:
+            raise DistutilsPlatformError('pip returned unsuccessfully')
