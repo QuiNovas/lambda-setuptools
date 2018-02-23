9c2cac96e9b6bf2e495d63bd9bdd55f716eff03d
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-02-23 14:13:17 -0600

    utf-8 encoding

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Convert the README.md to README.rst
-with open('README.rst', 'w') as readme:
+with open('README.rst', 'w', encoding='utf-8') as readme:
     readme.write(pypandoc.convert('README.md', 'rst', format='markdown'))
 
 setup(
